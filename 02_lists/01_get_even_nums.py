26eb1bfeca456f77096d570aa0b5e18e55b5146e
P-Copley <pkcopley@gmail.com>
2023-08-25 14:15:00 +0100

    lists: setup exercise format

---
+++ 02_lists/01_get_even_nums.py
@@ -0,0 +1,13 @@
+'''
+Write a function, get_even_nums, that takes an list of numbers (nums) and returns a list of only the even numbers.
+
+getEvenNumbers([1, 2, 3]) // returns [2]
+'''
+
+def get_even_nums(nums):
+    # your code here
+    pass
+
+def test_returns_an_empty_list_for_no_numbers():
+    return_value = get_even_nums([])
+    assert return_value == []
