eaa7956ba48cbdf465bdf88e93b68d645e3faf8a
P-Copley <pkcopley@gmail.com>
2023-08-25 14:38:48 +0100

    lists: pep8 format

--- 02_lists/02_get_items_longer_than.py
+++ 02_lists/02_get_items_longer_than.py
@@ -9,21 +9,26 @@ get_items_longer_than(['a','bb','ccc'], 2) //returns ['ccc'];
 get_items_longer_than(['a','bb','ccc'], 4) //returns [];
 '''
 
+
 def get_items_longer_than(strs, max_len):
     # your code here
     pass
+
 
 def test_returns_an_empty_list_for_no_strings():
     return_value = get_items_longer_than([], 1)
     assert return_value == []
 
+
 def test_includes_strings_longer_than_the_max_len():
     return_value = get_items_longer_than(['a', 'bb'], 0)
     assert return_value == ['a', 'bb']
 
+
 def test_excludes_strings_shorter_than_the_max_len():
     return_value = get_items_longer_than(['a', 'bb'], 3)
     assert return_value == []
+
 
 def test_excludes_strings_equal_to_the_max_len():
     return_value = get_items_longer_than(['a', 'bb', 'ccc'], 2)
