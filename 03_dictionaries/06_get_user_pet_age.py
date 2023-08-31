6428a8999595e128694077c231ded154fcbf1d75
Joe Mulvey <joe.mulvey@northcoders.com>
2023-08-31 15:25:28 +0100

    [DEC-140] add dictionary exercise get user pet age

---
+++ 03_dictionaries/06_get_user_pet_age.py
@@ -0,0 +1,67 @@
+"""
+Write a function that takes a `user` dictionary that looks like this:
+
+{
+  'name': "Tom",      
+  'age': 26,
+  'pet': {
+    'name': "Barney",
+    'age': 6,
+    'type': "good boy"
+  }
+}
+
+The dictionary is nested; there are dictionarys paired to keys on the user dictionary.
+      
+The function should the access the age property in the nested pet dictionary and return the value.
+
+
+user = {
+  'name': "Carrie",      
+  'age': 26,
+  'pet': {
+    'name': "Pixie",
+    'age': 4,
+    'type': "gremlin"
+  }
+}
+
+get_user_pet_age(user) # returns 4
+"""
+
+def get_user_pet_age(user):
+    # Your code here
+    pass
+
+
+def test_returns_none_if_no_pet():
+    user = {
+      'name': "Carrie",      
+      'age': 26
+    }
+    assert get_user_pet_age(user) is None
+
+
+def test_returns_none_if_no_pet_age():
+    user = {
+      'name': "Carrie",      
+      'age': 26,
+      'pet': {
+        'name': "Pixie",
+        'type': "gremlin"
+      }
+    }
+    assert get_user_pet_age(user) is None
+
+  
+def test_gets_pet_age():
+    user = {
+      'name': "Carrie",      
+      'age': 26,
+      'pet': {
+        'name': "Pixie",
+        'age': 4,
+        'type': "gremlin"
+      }
+    }
+    assert get_user_pet_age(user) == 4
