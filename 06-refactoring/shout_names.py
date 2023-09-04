54995dc035c7b4318e7a73346fcd0d4018314bdb
Joe Mulvey <joe.mulvey@northcoders.com>
2023-09-04 14:24:23 +0100

    [DEC-137] Add refactoring material shout names

---
+++ 06-refactoring/shout_names.py
@@ -0,0 +1,32 @@
+"""
+`shout_names` is a function that takes a list of names and returns a list containing all of the names in capital letters followed by an exclamation mark "1".
+
+Refactor `shout_names` to use a list comprehension.
+
+If you run the function, it should pass all of the tests. 
+"""
+
+def shout_names(names):
+    shouted_names = []
+    for name in names:
+        shouted_names.append(name.upper() + '!')
+    return shouted_names
+
+
+# Do not change any code below this line
+
+def test_returns_a_list():
+    assert type(shout_names([])) == list
+
+
+def test_no_names_produces_empty_list():
+    assert shout_names([]) == []
+
+
+def test_shouts_names():
+    names = ['Carrie', 'Diya', 'Kyle', 'Christian']
+    expected = ['CARRIE!', 'DIYA!', 'KYLE!', 'CHRISTIAN!']
+    assert shout_names(names) == expected
+    names = ['Maddie', 'Bethan', 'Andrea', 'Jade']
+    expected = ['MADDIE!', 'BETHAN!', 'ANDREA!', 'JADE!']
+    assert shout_names(names) == expected
