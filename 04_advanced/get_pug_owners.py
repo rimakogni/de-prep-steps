b4e47c23d5b05e3c9fae3bb885f5003e453f6da5
P-Copley <pkcopley@gmail.com>
2023-09-04 15:32:25 +0100

    advanced: format get_pug_owners

--- 04_advanced/get_pug_owners.py
+++ 04_advanced/get_pug_owners.py
@@ -3,18 +3,18 @@ Write a function takes a list of dictionaries describing `dogs` and returns a li
 
 
 get_pug_owners([
-      {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
-      {'name': 'Max', 'breed': 'Pug', 'owner': 'Izzi'},
-      {'name': 'Poppy', 'breed': 'Pug', 'owner': 'Anat'}
-    ]) // returns ['Izzi', 'Anat']
+    {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
+    {'name': 'Max', 'breed': 'Pug', 'owner': 'Izzi'},
+    {'name': 'Poppy', 'breed': 'Pug', 'owner': 'Anat'}
+]) # returns ['Izzi', 'Anat']
 
 get_pug_owners([
-      {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
-    ]) // returns []
+    {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
+]) # returns []
 
-get_pug_owners([]) // returns []
-
+get_pug_owners([]) # returns []
 """
+
 
 def get_pug_owners(dogs):
     # Your code here
@@ -27,29 +27,29 @@ def test_empty_list():
 
 def test_single_invalid_dog():
     assert get_pug_owners([
-      {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
+        {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
     ]) == []
 
 
 def test_single_valid_dog():
     assert get_pug_owners([
-      {'name': 'Beatrice', 'breed': 'Pug', 'owner': 'Tom'},
+        {'name': 'Beatrice', 'breed': 'Pug', 'owner': 'Tom'},
     ]) == ['Tom']
 
 
 def test_many_invalid_dogs():
     dogs = [
-      {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
-      {'name': 'Max', 'breed': 'Dalmation', 'owner': 'Izzi'},
-      {'name': 'Poppy', 'breed': 'Alsatian', 'owner': 'Anat'}
+        {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
+        {'name': 'Max', 'breed': 'Dalmation', 'owner': 'Izzi'},
+        {'name': 'Poppy', 'breed': 'Alsatian', 'owner': 'Anat'}
     ]
     assert get_pug_owners(dogs) == []
 
 
 def test_mixed_list():
     dogs = [
-      {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
-      {'name': 'Max', 'breed': 'Pug', 'owner': 'Izzi'},
-      {'name': 'Poppy', 'breed': 'Pug', 'owner': 'Anat'}
+        {'name': 'Beatrice', 'breed': 'Lurcher', 'owner': 'Tom'},
+        {'name': 'Max', 'breed': 'Pug', 'owner': 'Izzi'},
+        {'name': 'Poppy', 'breed': 'Pug', 'owner': 'Anat'}
     ]
     assert get_pug_owners(dogs) == ['Izzi', 'Anat']
