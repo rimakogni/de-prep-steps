fe2fa3e7902c539b9a38fac5090f1bf39d0ef953
Joe Mulvey <joe.mulvey@northcoders.com>
2023-09-04 11:10:56 +0100

    [DEC-132] Add debugging material split string

---
+++ 05_debugging/split_string.py
@@ -0,0 +1,15 @@
+"""
+split_string is a function that takes a string and returns an list containing all of the individual characters as separate elements.
+
+Unfortunately, it looks like the function is currently throwing a TypeError.
+
+Can you work out what needs changing to make the function pass all the tests?
+"""
+
+def split_string():
+    return list(str)
+
+def test_it_splits_strings():
+    assert split_string('string') == ['s', 't', 'r', 'i', 'n', 'g']
+    assert split_string('help') == ['h', 'e', 'l', 'p']
+    assert split_string('Northcoders') == ['N', 'o', 'r', 't', 'h', 'c', 'o', 'd', 'e', 'r', 's']
