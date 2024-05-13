d33c489657348cb362b1abcb88fea6ce2eb1f9f6
DCrawley94 <danikacrawley.dev@gmail.com>
2024-05-13 17:00:26 +0100

    add first few tests for Check

--- test_api/checks.py
+++ test_api/checks.py
@@ -6,7 +6,7 @@ class SkipCheck:
     def when_called_with(self, *args):
         pass
 
-    def is_not(self, orginal_data):
+    def is_not(self, original_data):
         pass
 
     def is_type(self, data_type):
@@ -14,3 +14,40 @@ class SkipCheck:
 
     def returns(self, return_value):
         print(f"Test {self.title}, {self.func.__name__}(): skipping test...")
+
+
+class Check:
+    def __init__(self, func, title):
+        self.func = func
+        self.title = title
+
+    def when_called_with(self, *args):
+        self.args = args
+        return self
+
+    def is_not(self, original_data):
+        self._set_return_value()
+
+        try:
+            assert self.return_value is not original_data
+            print(f"Test {self.title}, {self.func.__name__}(): Test passed")
+        except AssertionError:
+            print('Return value should be a new object')
+            raise
+ 
+
+    def is_type(self, data_type):
+        # make use of _set_return_value?
+        # print here
+        pass
+
+    def _set_return_value(self):
+        if (hasattr(self, "args")):
+            self.return_value = self.func(*self.args)
+        else:
+            self.return_value = self.func()
+
+
+    def returns(self):
+        # print here
+        pass
---
+++ test_api/test_checks.py
@@ -0,0 +1,172 @@
+from checks import SkipCheck, Check
+import pytest
+
+
+class TestSkipCheck:
+
+    def test_skipCheck_is_initalised_with_a_function_and_title(self):
+        def add(num):
+            return num + num
+
+        skip = SkipCheck(add, "add")
+
+        assert skip.func == add
+        assert skip.title == "add"
+
+    def test_when_called_with_returns_None(self):
+        def add(num):
+            return num + num
+
+        skip = SkipCheck(add, "add")
+
+        assert skip.when_called_with(2) is None
+
+    def test_is_not_returns_None(self):
+        def add(num):
+            return num + num
+
+        skip = SkipCheck(add, "add")
+
+        assert skip.is_not({"lucky_number": 13}) is None
+
+    def test_is_type_returns_None(self):
+        def add(num):
+            return num + num
+
+        skip = SkipCheck(add, "add")
+
+        assert skip.is_type(str) is None
+
+    def test_returns_should_return_skip_test_message(self, capsys):
+        test_title = "returns 8"
+
+        def add(num):
+            return num + num
+
+        skip = SkipCheck(add, test_title)
+        skip.returns(4)
+
+        captured = capsys.readouterr()
+        log_message = f"Test {test_title}, {add.__name__}(): skipping test..."
+
+        assert log_message in captured.out
+
+
+class TestCheck:
+
+    def test_check_is_initialised_with_a_function_and_title(self):
+        def add(num):
+            return num + num
+
+        check = Check(add, "add")
+
+        assert check.func == add
+        assert check.title == "add"
+
+    def test_when_called_with_set_args_attr_single_arg(self):
+        def add(num):
+            return num + num
+
+        check = Check(add, "add")
+
+        check.when_called_with(1)
+
+        assert check.args == (1,)
+
+    def test_when_called_with_set_args_attr_multi_args(self):
+        def add(num_1, num_2):
+            return num_1 + num_2
+
+        check = Check(add, "add")
+
+        check.when_called_with(1, 2)
+
+        # assert 1 in check.args
+        # assert 2 in check.args
+        assert check.args == (1,2)
+
+    def test_when_called_with_returns_self(self):
+        def add(num_1, num_2):
+            return num_1 + num_2
+
+        check = Check(add, "add")
+
+        returned = check.when_called_with(1, 2)
+
+        assert returned is check
+
+
+    def test_set_return_value_no_args(self):
+        def say_hello():
+            return 'hello'
+
+        check = Check(say_hello, "says hello title")
+
+        check._set_return_value()
+
+        assert check.return_value == 'hello'
+
+    def test_set_return_value_with_args(self):
+        def add(num_1, num_2):
+            return num_1 + num_2
+
+        check = Check(add, "add")
+
+        returned = check.when_called_with(1, 2)
+
+        check._set_return_value()
+
+        assert check.return_value == 3
+
+    def test_is_not_raises_assertion_error_when_return_value_IS_the_given_object(self):
+        def return_list(some_list):
+            return some_list
+
+        check = Check(return_list, 'returns different list')
+
+        test_list = [1,2,3]
+
+        with pytest.raises(AssertionError):
+            check.when_called_with(test_list).is_not(test_list)
+            
+        
+    def test_is_not_prints_helpful_message_when_return_value_IS_the_given_object(self, capsys):
+        def return_list(some_list):
+            return some_list
+
+        check = Check(return_list, 'returns different list')
+
+        test_list = [1,2,3]
+
+        with pytest.raises(AssertionError):
+            check.when_called_with(test_list).is_not(test_list)
+
+            captured = capsys.readouterr()
+            log_message = "Return value should be a new object"
+
+            assert log_message in captured.out
+
+        # try:
+        #     check.when_called_with(test_list).is_not(test_list)
+        #     raise Exception("Function should raise AssertionError")
+        # except AssertionError:
+        #     captured = capsys.readouterr()
+        #     log_message = "Return value should be a new object"
+
+        #     assert log_message in captured.out
+
+    def test_is_not_prints_helpful_message_when_return_value_IS_NOT_the_given_object(self, capsys):
+        def return_list(some_list):
+            return [el for el in some_list]
+
+        test_title = 'returns different list'
+        check = Check(return_list, test_title)
+
+        test_list = [1,2,3]
+
+        check.when_called_with(test_list).is_not(test_list)
+
+        captured = capsys.readouterr()
+        log_message = f"Test {test_title}, {return_list.__name__}(): Test passed"
+
+        assert log_message in captured.out
