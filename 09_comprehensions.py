0805b3aad0bbf8ae667f3278fdcaa7df3c58a628
hannahburks <hannah.burks@northcoders.com>
2025-02-24 15:20:56 +0000

    feat: add switch name and ID comprehensions exercise

--- 09_comprehensions.py
+++ 09_comprehensions.py
@@ -1,6 +1,6 @@
 #Task 1:  1 Create Greeting Strings (LIST) ✅
 #Task 2: 4 Increment Even Numbers (LIST)✅
-#Task 3: 8 Switch Name and ID (Dict)
+#Task 3: 8 Switch Name and ID (Dict)✅
 #Task 4: 11 Find Average Games (Dict)
 #ßTask 5: 12 Get Unique Departments (SET)
 
@@ -113,6 +113,106 @@ def test_increment_even_numbers_mixed_even_and_odd():
         5,
     ], format_err_msg([1, 3, 3, 5, 5], increment_even_numbers([1, 2, 3, 4, 5]))
 
+"""
+### switch_name_and_id ###
+The function should accept a dictionary. In this dictionary, each key is a staff member's name and the value will be their ID number. The function should return a dictionary with the keys and values switched around. It should work in the following way:
+
+```py
+switch_name_and_id({"Alex": "a7d29w"}) # {"a7d29w": "Alex"}
+switch_name_and_id({"Kyle": "a7d29w", "Danika": "z2r51e", "Liam": "p3f44m"}) 
+
+# {"a7d29w": "Kyle", "z2r51e": "Danika", "p3f44m": "Liam"}
+```
+
+This function is currently working exactly as it should, but it could be much more concise.
+
+Please refactor `switch_name_and_id` to use a **dictionary comprehension**.
+
+"""
+
+def switch_name_and_id(data):
+    switched_data = {}
+
+    for record in data.items():
+        switched_data[record[1]] = record[0]
+
+    return switched_data
+
+
+@skip_test
+def test_switch_name_and_id_passed_empty_dict_returns_empty_dict_():
+    assert switch_name_and_id({}) == {}, format_err_msg(
+        {}, switch_name_and_id({})
+    )
+
+
+@skip_test
+def test_switch_name_and_id_returns_dict_with_single_swapped_key_and_value():
+    assert switch_name_and_id({"Alex": "a7d29w"}) == {
+        "a7d29w": "Alex"
+    }, format_err_msg(
+        {"a7d29w": "Alex"}, switch_name_and_id({"Alex": "a7d29w"})
+    )
+    assert switch_name_and_id({"Chon": "z2r51e"}) == {
+        "z2r51e": "Chon"
+    }, format_err_msg(
+        {"z2r51e": "Chon"}, switch_name_and_id({"Chon": "z2r51e"})
+    )
+    assert switch_name_and_id({"Cat": "p3f44m"}) == {
+        "p3f44m": "Cat"
+    }, format_err_msg({"p3f44m": "Cat"}, switch_name_and_id({"Cat": "p3f44m"}))
+
+
+@skip_test
+def test_switch_name_and_id_returns_dict_with_swapped_keys_and_values():
+    assert switch_name_and_id({"Alex": "a7d29w", "Chon": "z2r51e"}) == {
+        "a7d29w": "Alex",
+        "z2r51e": "Chon",
+    }, format_err_msg(
+        {"a7d29w": "Alex", "z2r51e": "Chon"},
+        switch_name_and_id({"Alex": "a7d29w", "Chon": "z2r51e"}),
+    )
+    assert switch_name_and_id(
+        {"Simon": "a7d29w", "Joe": "z2r51e", "Paul": "p3f44m"}
+    ) == {
+        "a7d29w": "Simon",
+        "z2r51e": "Joe",
+        "p3f44m": "Paul",
+    }, format_err_msg(
+        {"a7d29w": "Simon", "z2r51e": "Joe", "p3f44m": "Paul"},
+        switch_name_and_id(
+            {"Simon": "a7d29w", "Joe": "z2r51e", "Paul": "p3f44m"}
+        ),
+    )
+    assert switch_name_and_id(
+        {
+            "Kyle": "a7d29w",
+            "Danika": "z2r51e",
+            "Liam": "p3f44m",
+            "Cat": "q4r55t",
+        }
+    ) == {
+        "a7d29w": "Kyle",
+        "z2r51e": "Danika",
+        "p3f44m": "Liam",
+        "q4r55t": "Cat",
+    }, format_err_msg(
+        {
+            "a7d29w": "Kyle",
+            "z2r51e": "Danika",
+            "p3f44m": "Liam",
+            "q4r55t": "Cat",
+        },
+        switch_name_and_id(
+            {
+                "Kyle": "a7d29w",
+                "Danika": "z2r51e",
+                "Liam": "p3f44m",
+                "Cat": "q4r55t",
+            }
+        ),
+    )
+
 if __name__ == "__main__":
     test_create_greeting_strings_return_empty_list()
     test_create_greeting_strings_greets_single_name_in_list()
@@ -121,3 +221,6 @@ if __name__ == "__main__":
     test_increment_even_numbers_increments_even_numbers()
     test_increment_even_numbers_ignores_odd_numbers()
     test_increment_even_numbers_mixed_even_and_odd()
+    test_switch_name_and_id_passed_empty_dict_returns_empty_dict_()
+    test_switch_name_and_id_returns_dict_with_single_swapped_key_and_value()
+    test_switch_name_and_id_returns_dict_with_swapped_keys_and_values()
