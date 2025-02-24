535e42601bd3c2cf3256eb22cd1ca99102eafe1b
hannahburks <hannah.burks@northcoders.com>
2025-02-24 15:15:08 +0000

    feat: add 2nd exercise for comprehensions

--- 09_comprehensions.py
+++ 09_comprehensions.py
@@ -1,15 +1,13 @@
-#Task 1:  1 Create Greeting Strings (LIST)
-#Task 2: 4 Increment Even Numbers (LIST)
+#Task 1:  1 Create Greeting Strings (LIST) ✅
+#Task 2: 4 Increment Even Numbers (LIST)✅
 #Task 3: 8 Switch Name and ID (Dict)
 #Task 4: 11 Find Average Games (Dict)
 #ßTask 5: 12 Get Unique Departments (SET)
 
-
-# Task 1: Create Greeting Strings
 from test_api.checks import run_test, skip_test, format_err_msg
-## Instructions
+
 """
-###Create Greeting Strings###
+### create_greeting_strings ###
 
 First, we will take a look at **list comprehensions**!
 
@@ -65,7 +63,61 @@ def test_create_greeting_strings_greets_all_names_in_list():
         create_greeting_strings(test_list) == expected_result
     ), format_err_msg(expected_result, create_greeting_strings(test_list))
 
+
+"""
+### increment_even_numbers ###
+
+This function should accept a list of numbers. It should increment **only** the even numbers and return the list with those changes:
+
+```py
+increment_even_numbers([ 2, 4, 6 ]) # [ 3, 5, 7 ]
+increment_even_numbers([ 1, 3, 5 ]) # [ 1, 3, 5 ]
+increment_even_numbers([ 1, 2, 3, 4, 5 ]) # [ 1, 3, 3, 5, 5 ]
+```
+There is no existing code for you to refactor, instead you should solve `increment_even_numbers` using a **list comprehension**.
+
+"""
+
+def increment_even_numbers(number_list):
+    pass
+
+
+@skip_test
+def test_increment_even_numbers_return_empty_list():
+    assert increment_even_numbers([]) == [], format_err_msg(
+        [], increment_even_numbers([])
+    )
+
+
+@skip_test
+def test_increment_even_numbers_increments_even_numbers():
+    assert increment_even_numbers([2, 4, 6]) == [3, 5, 7], format_err_msg(
+        [3, 5, 7], increment_even_numbers([2, 4, 6])
+    )
+
+
+@skip_test
+def test_increment_even_numbers_ignores_odd_numbers():
+    assert increment_even_numbers([1, 3, 5]) == [1, 3, 5], format_err_msg(
+        [1, 3, 5], increment_even_numbers([1, 3, 5])
+    )
+
+
+@skip_test
+def test_increment_even_numbers_mixed_even_and_odd():
+    assert increment_even_numbers([1, 2, 3, 4, 5]) == [
+        1,
+        3,
+        3,
+        5,
+        5,
+    ], format_err_msg([1, 3, 3, 5, 5], increment_even_numbers([1, 2, 3, 4, 5]))
+
 if __name__ == "__main__":
     test_create_greeting_strings_return_empty_list()
     test_create_greeting_strings_greets_single_name_in_list()
     test_create_greeting_strings_greets_all_names_in_list()
+    test_increment_even_numbers_return_empty_list()
+    test_increment_even_numbers_increments_even_numbers()
+    test_increment_even_numbers_ignores_odd_numbers()
+    test_increment_even_numbers_mixed_even_and_odd()
